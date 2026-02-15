4638254dcb62a378be280ebff34109f83f94efd1
rybmks <rybalkomaks3@gmail.com>
2026-02-15 18:37:46 +0100

    add loss plot

--- lab1/main.py
+++ lab1/main.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 import numpy as np
+import matplotlib.pyplot as plt
 
 X = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
 Y = np.array([0, 1, 1, 0])
@@ -10,16 +11,29 @@ def main():
         tf.keras.layers.Dense(1, activation="sigmoid")
     ])
 
-    model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.05), loss="binary_crossentropy", metrics=['accuracy'])
-    model.fit(X, Y, epochs=100)
+    model.compile(
+        optimizer=tf.keras.optimizers.Adam(learning_rate=0.05),
+        loss="binary_crossentropy",
+        metrics=['accuracy']
+    )
 
-    loss, accuracy = model.evaluate(X, Y)
+    history = model.fit(X, Y, epochs=100, verbose=0)
+
+    loss, accuracy = model.evaluate(X, Y, verbose=0)
     print("loss:", loss)
     print("accuracy:", accuracy)
 
     prediction = model.predict(X)
     for input, pred in zip(X, prediction):
         print(input, "Out rounded:", round(pred[0]), "Out:", pred[0])
+
+    plt.figure()
+    plt.plot(history.history['loss'])
+    plt.title("Loss")
+    plt.xlabel("Epoch")
+    plt.ylabel("Loss")
+    plt.grid()
+    plt.show()
 
 
 if __name__ == "__main__":
